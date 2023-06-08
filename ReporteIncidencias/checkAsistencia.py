4bb6efedf7819b7e4dd8c03bcabc0680f51d5762
Gonzalo Fernández <gonchfm@gmail.com>
2023-06-08 19:10:03 +0200

    Optimización de código

--- ReporteIncidencias/checkAsistencia.py
+++ ReporteIncidencias/checkAsistencia.py
@@ -5,17 +5,14 @@ from datetime import timedelta
 import json
 import sys
 
-
-
-
+#Conexión con la base de datos
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="Elgoncho8!FM",
-  #database="mydatabase"
 )
 
-
+#Funcion para obtener el cuatrimestre dado un dia y un mes
 def checkCuatri(mes, dia):
     if(mes == 9 or mes == 10 or mes == 11 or mes == 12):
         cuatri = 1
@@ -31,30 +28,34 @@ def checkCuatri(mes, dia):
 
     return cuatri
 
-
+#Funcion que genera un fichero de salida con el registro de las incidencias surgidas en un dia
 def asistenciaProfesores(fecha, semana):
-    #fecha = datetime.datetime(2023, 5, 5)
+    print("Funcion dia")
     month = fecha.month
     year = fecha.year
     day = fecha.day
+    dia = fecha.weekday()
     cuatri = checkCuatri(month, day)
     
     #Creacion del documento JSON
     reg_Incidencias = {}
     incidenciasList = []
-    reg_Incidencias["fecha"] = str(fecha)
+    fecha_str = str(fecha)
+    fecha_incidencia = fecha_str.split(" ")[0]
+    reg_Incidencias["fecha"] = str(fecha_incidencia)
     
 
     mycursor = mydb.cursor()
-    parameters = (cuatri, day)
+    parameters = (cuatri, dia)
     #Query para buscar todas las horas docentes que hay en el dia seleccionado
     query = "SELECT aula,hora_inicio,profe_imparte,idAsignatura,idGrupo FROM fdi_ucm.info_docente WHERE cuatrimestre = %s AND dia = %s"
     mycursor.execute(query, parameters)
     myresult = mycursor.fetchall()
-    #Se recorren todas las horas
-    for x in myresult:
-        print(x)
-        parameters = (x[0], x[2], fecha)
+    print(myresult)
+    #Se recorren todas las clases que se imparten durante el dia
+    for lesson in myresult:
+        print(lesson)
+        parameters = (lesson[0], lesson[2], fecha)
         #Query a la tabla de asistencia para comprobar que el profesor ha asistido en el horario
         query = "SELECT * FROM fdi_ucm.asistencia WHERE aula = %s AND idUsuario = %s AND fecha = %s"
         mycursor.execute(query, parameters)
@@ -63,137 +64,131 @@ def asistenciaProfesores(fecha, semana):
         if len(myresult) > 0:
             time = myresult[0][4]
             aux = str(time)
-            hour = int(aux[0:2]) #Substring con las horas
-            minutes = int(aux[3:5]) #Substring con los minutos
-            if hour == x[1]:
+            hour = int(aux.split(":")[0]) #Substring con las horas
+            minutes = int(aux.split(":")[1]) #Substring con los minutos
+            incidencia = {}
+            if str(hour) == lesson[1]:
                 print("Asistencia correcta")
                 #Se comprueba si el profesor ha llegado tarde
+                
                 if minutes > 5 and minutes < 45:
-                    parameters = (x[2])
+                    print("Con retraso")
+                    parameters = []
+                    parameters.append(lesson[2])
                     query = "SELECT nombre FROM fdi_ucm.usuarios WHERE idUsuario = %s"
                     mycursor.execute(query, parameters)
                     myresult = mycursor.fetchall()
-                    incidencia = {}
-                    incidencia["idProfesor"] = x[2]
-                    incidencia["nombreProfesor"] = myresult[0][0]
-                    incidencia["aula"] = x[0]
-                    incidencia["idAsignatura"] = x[3]
-                    incidencia["idGrupo"] = x[4]
-                    incidencia["motivo"] = "Retraso de " + str(minutes) + " minutos"
+                    rellenarIncidencia(incidencia, lesson[2], myresult[0][0], lesson[0], lesson[3], lesson[4], "Retraso de " + str(minutes) + " minutos")
                     
             else:
                 if minutes > 45:
                     hour = hour + 1
-                if str(hour) == x[1]:
+                if str(hour) == lesson[1]:
                     print("Asistencia correcta")
-                
+
                 #Si se comprueba que no asistió a esa hora, se introduce en el json de incidencias                
                 else:
                     print("Asistencia incorrecta")
-                    parameters = (x[2])
-                    query = "SELECT nombre FROM fdi_ucm.usuarios WHERE idUsuario = %s"
-                    mycursor.execute(query, parameters)
-                    myresult = mycursor.fetchall()
-                    incidencia = {}
-                    incidencia["idProfesor"] = x[2]
-                    incidencia["nombreProfesor"] = myresult[0][0]
-                    incidencia["aula"] = x[0]
-                    incidencia["idAsignatura"] = x[3]
-                    incidencia["idGrupo"] = x[4]
-                    incidencia["motivo"] = "No asiste"
-                    #Ahora se comprueba si ha ido un profesor suplente
-                    parameters = (x[0], fecha, 1)
-                    query = "SELECT idUsuario FROM fdi_ucm.asistencia WHERE aula = %s AND fecha = %s AND codigo_incidencia = %s"
-                    mycursor.execute(query, parameters)
-                    myresult = mycursor.fetchall()
-                    if len(myresult) > 0:
-                        parameters = myresult[0][0]
-                        query = "SELECT nombre FROM fdi_ucm.usuarios WHERE idUsuario = %s"
-                        mycursor.execute(query, parameters)
-                        myresult = mycursor.fetchall()
-                        incidencia["sustituto"] = myresult[0][0]
-    
-                    incidenciasList.append(incidencia)
+                    incidencia = profesorNoAsiste(incidencia, lesson, fecha)
+            
+            if incidencia:
+                incidenciasList.append(incidencia)
                     
    
         #Si no hay ningun resultado, se introduce en el json de incidencias
         else:
             print("Asistencia incorrecta")
-            parameters = []
-            parameters.append(x[2])
-            query = "SELECT nombre FROM fdi_ucm.usuarios WHERE idUsuario = %s"
-            mycursor.execute(query, parameters)
-            myresult = mycursor.fetchall()
             incidencia = {}
-            incidencia["idProfesor"] = x[2]
-            incidencia["nombreProfesor"] = myresult[0][0]
-            incidencia["aula"] = x[0]
-            incidencia["idAsignatura"] = x[3]
-            incidencia["idGrupo"] = x[4]
-            incidenciasList.append(incidencia)
-            #Ahora se comprueba si ha ido un profesor suplente
-            parameters = (x[0], fecha, 1)
-            query = "SELECT idUsuario FROM fdi_ucm.asistencia WHERE aula = %s AND fecha = %s AND codigo_incidencia = %s"
-            mycursor.execute(query, parameters)
-            myresult = mycursor.fetchall()
-            if len(myresult) > 0:
-                parameters = myresult[0][0]
-                query = "SELECT nombre FROM fdi_ucm.usuarios WHERE idUsuario = %s"
-                mycursor.execute(query, parameters)
-                myresult = mycursor.fetchall()
-                incidencia["sustituto"] = myresult[0][0]
-
+            incidencia = profesorNoAsiste(incidencia, lesson, fecha)
             incidenciasList.append(incidencia)
             
     reg_Incidencias["incidencias"] = incidenciasList
     json_data = json.dumps(reg_Incidencias)
     
     if semana == 0:
-        dateStr = str(fecha)
-        dateStr = dateStr[0:10]
-        fileName = dateStr + "_registroIncidencias.json"
+        fileName = fecha_incidencia + "_registroIncidencias.json"
         with open(fileName, 'w') as outfile:
             outfile.write(json_data)
     return incidenciasList
     
     
     
-
-def asistenciaProfesoresSemana(lunesInicio):
+#Funcion que genera un fichero de salida con el registro de las incidencias surgidas en una semana
+def asistenciaProfesoresSemana(fechaInicial):
     reg_total = []
-    dateStr = str(lunesInicio)
-    dateStr = dateStr[0:10]
+    dateStr = str(fechaInicial)
+    dateStr = dateStr.split(" ")[0]
     fileName = "Semana" + dateStr + "_registroIncidencias.json"
     
     for i in range(5):
         incidencias = []
         reg_Incidencias = {}
-        reg_Incidencias["fecha"] = str(lunesInicio)
-        incidencias = asistenciaProfesores(lunesInicio, 1)
+        fecha_str = str(fechaInicial)
+        fecha_str = fecha_str.split(" ")[0]
+        reg_Incidencias["fecha"] = fecha_str
+        incidencias = asistenciaProfesores(fechaInicial, 1)
         reg_Incidencias["incidencias"] = incidencias
         reg_total.append(reg_Incidencias)
-        lunesInicio += timedelta(days=1)
+        fechaInicial += timedelta(days=1)
 
     json_data = json.dumps(reg_total)    
     
     print(fileName)
     with open(fileName, 'w') as outfile:
         outfile.write(json_data)
+
+#Funcion auxiliar para introducir datos en la estructura json
+def rellenarIncidencia(incidencia, idP, nombre, aula, idA, idG, motivo):
+    incidencia["idProfesor"] = idP
+    incidencia["nombreProfesor"] = nombre
+    incidencia["aula"] = aula
+    incidencia["idAsignatura"] = idA
+    incidencia["idGrupo"] = idG
+    incidencia["motivo"] = motivo
     
+#Funcion auxiliar que cubre la funcionalidad en el caso que un profesor no haya asistido a su clase
+def profesorNoAsiste(incidencia, lesson, fecha):
+    mycursor = mydb.cursor()
+    parameters = []
+    parameters.append(lesson[2])
+    query = "SELECT nombre FROM fdi_ucm.usuarios WHERE idUsuario = %s"
+    mycursor.execute(query, parameters)
+    myresult = mycursor.fetchall()
+    rellenarIncidencia(incidencia, lesson[2], myresult[0][0], lesson[0], lesson[3], lesson[4], "No asiste")
+    #Ahora se comprueba si ha ido un profesor suplente
+    parameters = (lesson[0], fecha)
+    print("parameters " + str(lesson[0]))
+    query = "SELECT idUsuario, hora FROM fdi_ucm.asistencia WHERE aula = %s AND fecha = %s"
+    mycursor.execute(query, parameters)
+    myresult = mycursor.fetchall()
+    print(myresult)
+    if len(myresult) > 0:
+        horaSustituto = str(myresult[0][1])
+        horaSustituto = horaSustituto.split(":")[0]
+        print("Horasustituto = ")
+        print(horaSustituto)
+        if lesson[1] ==  horaSustituto:
+            parameters = (myresult[0][0],"0")
+            query = "SELECT nombre FROM fdi_ucm.usuarios WHERE idUsuario = %s AND cargo = %s"
+            mycursor.execute(query, parameters)
+            myresult = mycursor.fetchall()
+            incidencia["sustituto"] = myresult[0][0]
+    
+    return incidencia
 
 def main():   
     if len(sys.argv) == 3:
         if sys.argv[1] == "dia":
             print("Mostrando incidencias del dia: ")
             print(sys.argv[2])
-            date = datetime.strptime(sys.argv[2], '%d/%m/%Y')
+            date = datetime.strptime(sys.argv[2], '%Y-%m-%d')
             print(date)
             asistenciaProfesores(date, 0)
             
         elif sys.argv[1] == "semana":
             print("Mostrando incidencias de la semana del lunes: ")
             print(sys.argv[2])
-            date = datetime.strptime(sys.argv[2], '%d/%m/%Y')
+            date = datetime.strptime(sys.argv[2], '%Y-%m-%d')
             asistenciaProfesoresSemana(date)
 
     else:
